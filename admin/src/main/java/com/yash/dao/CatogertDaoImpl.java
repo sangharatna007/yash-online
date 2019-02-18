2d29555dc88312f4f04c1c413c2d2b2db2ffc08b
Sangharatna Chokha Davane <sangharatna.davane@yash.com>
2019-02-18 18:05:46 +0530

    made the code changes for admin controller to add new category

---
+++ admin/src/main/java/com/yash/dao/CatogertDaoImpl.java
@@ -0,0 +1,5 @@
+package com.yash.dao;
+
+public class CatogertDaoImpl {
+
+}
---
+++ admin/src/main/java/com/yash/model/Category.java
@@ -0,0 +1,24 @@
+package com.yash.model;
+
+public class Category {
+	
+	private String  category_id;
+	private String title;
+	
+	
+	public String getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(String category_id) {
+		this.category_id = category_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+
+}
